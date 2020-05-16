92be1b847aa8562813d8aaba62e0a5968317280d
Dailson Gomes <dailson.de.assis@gmail.com>
2020-05-16 13:11:03 -0300

    Resolve #2: Departmant class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,11 @@
  */
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 /**
  * Program 
@@ -18,9 +22,14 @@ public class Program {
 	 */
 	public static void main(String[] args) {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		Department obj = new Department(1, "book");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(1, "Dailson", "dailson@gmail.com",  new Date(), 2000.0, obj);
+		
+		
+		System.out.println(seller);
 
 	}
 
