8e80f7ae5a2266b9610d1e79c84a6cb7ebb11c69
Dailson Gomes <dailson.de.assis@gmail.com>
2020-05-16 12:46:14 -0300

    #1 - Department class

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package application;
+
+import model.entities.Department;
+
+/**
+ * Program 
+ *
+ * @author dailson
+ *
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "book");
+		
+		System.out.println(obj);
+
+	}
+
+}
