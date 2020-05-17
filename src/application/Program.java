92f1690d389c3b2f70a0aaa215a9b2deb4aef745
Dailson Gomes <dailson.de.assis@gmail.com>
2020-05-17 01:20:29 -0300

    Resolve #5: FindById

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,12 +3,8 @@
  */
 package application;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 /**
@@ -23,14 +19,10 @@ public class Program {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		Department obj = new Department(1, "book");
-		
-		Seller seller = new Seller(1, "Dailson", "dailson@gmail.com",  new Date(), 2000.0, obj);
-		
+				
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(1);
 		
 		System.out.println(seller);
 
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -3,6 +3,7 @@
  */
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJdbc;
 
 /**
@@ -14,6 +15,6 @@ import model.dao.impl.SellerDaoJdbc;
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJdbc();
+		return new SellerDaoJdbc(DB.getConnection());
 	}
 }
